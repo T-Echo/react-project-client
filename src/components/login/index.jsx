a009f82c849903a55b8dd29c624b3ef6ff12ed4f
tianshuang <279180694@qq.com>
2018-12-03 20:13:12 +0800

    完成注册、登陆界面

--- src/components/login/index.jsx
+++ src/components/login/index.jsx
@@ -1,9 +1,84 @@
 import React, {Component} from 'react';
+import { NavBar, List, InputItem, WhiteSpace,WingBlank, Button } from 'antd-mobile';
+import Logo from '../logo';
+
 
 class Login extends Component {
+
+  state = {
+    //有关页面变化的数据保存到状态
+    //isBossChecked : true,
+    username : '',
+    password:''
+  }
+
+  /*handleRadio = type => {
+   if (type === 'laoban'){
+   this.setState({
+   isBossChecked:true
+   })
+   }else {
+   this.setState({
+   isBossChecked : false
+   })
+   }
+   }*/
+
+  /*handleUserName = val => {
+   this.setState({
+   username : val
+   })
+   }
+   handlePassword = val => {
+   this.setState({
+   password : val
+   })
+   }
+   handleRepassword = val => {
+   this.setState({
+   repassword : val
+   })
+   }*/
+
+  //简化
+  handleChange = (type,value) => {
+    this.setState({
+      [type] : value
+    })
+  }
+
+  login = () => {
+    //收集表单数据
+    const {username, password} = this.state;
+    //发送ajax请求
+    console.log(username, password)
+  }
+
+  goRegister = () => {
+    //点击已有账户，去登陆页面，将地址切换为login
+    this.props.history.replace('/register');
+    //this.props.history.push('/login');会添加一条浏览记录
+  }
+
   render () {
+    const {laoban} = this.state;
     return (
-      <h1>login</h1>
+      <div>
+        <NavBar>硅谷直聘</NavBar>
+        <Logo />
+        <WingBlank>
+          <List>
+            {/*往定义的方法里面传参的方式*/}
+            <InputItem onChange={(val) => {this.handleChange('username',val)}}>用户名 :</InputItem>
+            <WhiteSpace/>
+            <InputItem onChange={(val) => {this.handleChange('password',val)}}>密&nbsp;&nbsp;&nbsp;码 :</InputItem>
+            <WhiteSpace/>
+            <Button type="primary" onClick={this.login}>登陆</Button>
+            <WhiteSpace/>
+            <Button onClick={this.goRegister}>没账户</Button>
+          </List>
+        </WingBlank>
+      </div>
     )
   }
 }
--- src/components/register/index.jsx
+++ src/components/register/index.jsx
@@ -1,9 +1,97 @@
 import React, {Component} from 'react';
+import { NavBar, List, InputItem, WhiteSpace,WingBlank, Radio, Button } from 'antd-mobile';
+import Logo from '../logo';
+
+const Item = List.Item;
+
 
 class Register extends Component {
+
+  state = {
+    //有关页面变化的数据保存到状态
+    //isBossChecked : true,
+    laoban : true,
+    username : '',
+    password:'',
+    repassword : ''
+  }
+
+  /*handleRadio = type => {
+    if (type === 'laoban'){
+      this.setState({
+        isBossChecked:true
+      })
+    }else {
+      this.setState({
+        isBossChecked : false
+      })
+    }
+  }*/
+
+  /*handleUserName = val => {
+    this.setState({
+      username : val
+    })
+  }
+  handlePassword = val => {
+    this.setState({
+      password : val
+    })
+  }
+  handleRepassword = val => {
+    this.setState({
+      repassword : val
+    })
+  }*/
+
+  //简化
+  handleChange = (type,value) => {
+    this.setState({
+      [type] : value
+    })
+  }
+
+  register = () => {
+    //收集表单数据
+    const {laoban, username, repassword, password} = this.state;
+    //发送ajax请求
+    console.log(laoban, username, repassword, password)
+  }
+
+  goLogin = () => {
+    //点击已有账户，去登陆页面，将地址切换为login
+    this.props.history.replace('/login');
+    //this.props.history.push('/login');会添加一条浏览记录
+  }
+
   render () {
+    const {laoban} = this.state;
     return (
-      <h1>Register</h1>
+      <div>
+        <NavBar>硅谷直聘</NavBar>
+        <Logo />
+        <WingBlank>
+          <List>
+            {/*往定义的方法里面传参的方式*/}
+            <InputItem onChange={(val) => {this.handleChange('username',val)}}>用户名 :</InputItem>
+            <WhiteSpace/>
+            <InputItem onChange={(val) => {this.handleChange('password',val)}}>密&nbsp;&nbsp;&nbsp;码 :</InputItem>
+            <WhiteSpace/>
+            <InputItem onChange={(val) => {this.handleChange('repassword',val)}}>确认密码 :</InputItem>
+            <WhiteSpace/>
+            <Item>
+              用户类型：&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              {/*往定义的方法里面传参的方式: 下面两种方式结果相同*/}
+              <Radio checked={!laoban} onChange={() => {this.handleChange('laoban',false)}}>大神</Radio>&nbsp;&nbsp;&nbsp;
+              <Radio checked={laoban} onChange={this.handleChange.bind(null,'laoban',true)}>老板</Radio>
+            </Item>
+            <WhiteSpace/>
+            <Button type="primary" onClick={this.register}>注册</Button>
+            <WhiteSpace/>
+            <Button onClick={this.goLogin}>已有账户</Button>
+          </List>
+        </WingBlank>
+      </div>
     )
   }
 }
